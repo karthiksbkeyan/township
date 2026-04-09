e5776d56542b4441a7834983037be85f9ccec7a3
karthiksbkeyan <karthiksbkeyan@gmail.com>
2026-04-09 13:49:35 +0530

    fix notices migration

--- township/settings.py
+++ township/settings.py
@@ -125,9 +125,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-
-if os.environ.get('CREATE_SUPERUSER') == 'True':
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
